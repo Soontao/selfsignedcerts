5ec184ef903e3a2fbc20a6528d00cc48c62c26ca
Robert Ragsdale <lesragsdale@gmail.com>
2015-11-05 13:10:14 -0500

    Complete main.bash and add working jks and client certs

--- main.bash
+++ main.bash
@@ -5,7 +5,7 @@ fi
 
 if [[ -z "$PASS" ]]
 then
-  PASS="password"
+  PASS="aicigaicig"
 fi
 
 rm -rf bin
@@ -15,8 +15,8 @@ cd bin
 mkdir ca
 cd ca
 
-openssl req -new -newkey rsa:1024 -nodes -out ca.csr -keyout ca.key -config ../../serverdefaults.txt
-openssl x509 -trustout -signkey ca.key -days 36500 -req -in ca.csr -out ca.pem
+openssl req -new -newkey rsa:1024 -days 36500 -nodes -out ca.csr -keyout ca.key -config ../../serverdefaults.txt
+openssl x509 -trustout -signkey ca.key -req -in ca.csr -out ca.pem -days 36500
 echo "02" > ca.srl
 
 cd ..
@@ -26,21 +26,22 @@ cd java
 keytool -genkey -dname "cn=$DOMAINNAME" -alias localhost -keyalg RSA -keystore keystore.jks -keypass $PASS -storepass $PASS
 keytool -import -keystore keystore.jks -file ../ca/ca.pem -alias selfca
 
-# cd ..
-# mkdir client
-# cd client
+cd ..
+mkdir client
+cd client
 
-# FILE="users.txt"
+FILE="../../users.txt"
 
-# oIFS="$IFS"
-# IFS=";"
+oIFS="$IFS"
+IFS=";"
 
-# while read name email; do
-# 	echo "--"
-# 	echo $name
-# 	echo $email
-# done < <(grep '' $FILE)
+while read name email; do
 
+  openssl req -new -newkey rsa:1024 -nodes -out ${name}.req -keyout ${name}.key -subj "/C=US/ST=Virginia/CN=${name}/EMAILADDRESS=aicig-${email}" -days 36500
+  openssl x509 -CA ../ca/ca.pem -CAkey ../ca/ca.key -CAserial ../ca/ca.srl -req -in ${name}.req -out ${name}.pem -days 36500
+  openssl pkcs12 -export -in ${name}.pem -inkey ${name}.key -out ${name}.p12 -name "${name}"
 
-# IFS="$oIFS"
-# unset oIFS
+done < <(grep '' $FILE)
+
+IFS="$oIFS"
+unset oIFS
